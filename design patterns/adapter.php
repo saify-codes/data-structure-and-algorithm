14e1482c802b5ada2a5eb6f709f6fcd0e63c3bf4
saify <sarfaraz.symits@gmail.com>
2026-02-17 20:57:30 +0500

    design patterns

---
+++ design patterns/adapter.php
@@ -0,0 +1,41 @@
+<?php
+
+interface PaymentGateway
+{
+    public function processPayment($amount);
+}
+
+interface EPaymentGateway
+{
+    public function process($amount);
+}
+
+
+
+class CardPayment implements PaymentGateway
+{
+    
+    public function processPayment($amount)
+    {
+        return "Paid {$amount} using Card\n";
+    }
+}
+
+class PaymentAdapter implements EPaymentGateway
+{
+    private $gateway;
+
+    public function __construct(PaymentGateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    public function process($amount)
+    {
+        return $this->gateway->processPayment($amount);
+    }
+}
+
+
+$obj = new PaymentAdapter(new CardPayment());
+echo $obj->process(100);
